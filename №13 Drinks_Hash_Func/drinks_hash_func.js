3ef66481cdcdbf4de166d3087e90c34c786bfca9
VladYE3 <vlad.verbitsky3@mail.ru>
2023-01-16 00:01:33 +0300

    New

---
+++ №13 Drinks_Hash_Func/drinks_hash_func.js
@@ -0,0 +1,51 @@
+function HashStorageFunc() {
+    const _storage = {};
+    this.addValue = function (key, value) {
+        _storage[key] = value;
+    }
+    this.getValue = function (key) {
+        if (key in _storage) {
+            return _storage[key];
+        } else {
+            return undefined;
+        }
+    }
+    this.deleteValue = function (key) {
+        if (key in _storage) {
+            delete _storage[key];
+            return true;
+        } else {
+            return false;
+        }
+    }
+    this.getKeys = function () {
+        return Object.keys(_storage);
+    }
+}
+const drinkStorage = new HashStorageFunc();
+document.getElementById('enter').onclick = function () {
+    const drinkName = prompt('Введите название напитка');
+    const drinkAlcohol = prompt('Напиток алкогольный?');
+    const drinkComposition = prompt('Введите рецепт напитка');
+    return drinkStorage.addValue(drinkName, {drinkAlcohol, drinkComposition});
+};
+document.getElementById('getInfo').onclick = function () {
+    const drinkName = prompt('Введите название напитка');
+    const info = drinkStorage.getValue(drinkName)
+    if (info) {
+        const {drinkAlcohol, drinkComposition} = info;
+        alert(`Напиток: ${drinkName}
+Напиток Алкогольный: ${drinkAlcohol} 
+Рецепт приготовления: ${drinkComposition}`
+        );
+    } else alert(`Напиток ${drinkName} не найден`);
+}
+document.getElementById('del').onclick = function () {
+    const drinkName = prompt('Введите название напитка');
+    if (drinkStorage.deleteValue(drinkName))
+        alert(`Информация о напитке ${drinkName} удалена`);
+    else alert(`Напиток ${drinkName} не найден`);
+}
+document.getElementById('getList').onclick = function () {
+    alert(`Имеется информация о напитках: ${drinkStorage.getKeys()}`);
+}
---
+++ №14 Drinks_Hash_Class/drinks_hash_class.js
@@ -0,0 +1,53 @@
+class HashStorageClass {
+    constructor() {
+        this.storage = {};
+    }
+    addValue(key, value) {
+        this.storage[key] = value;
+    }
+    getValue(key) {
+        if (key in this.storage) {
+            return this.storage[key];
+        } else {
+            return undefined;
+        }
+    }
+    deleteValue(key) {
+        if (key in this.storage) {
+            delete this.storage[key];
+            return true;
+        } else {
+            return false;
+        }
+    }
+    getKeys() {
+        return Object.keys(this.storage);
+    }
+}
+const drinkStorage = new HashStorageClass();
+document.getElementById('enter').onclick = () => {
+    const drinkName = prompt('Введите название напитка');
+    const drinkAlcohol = prompt('Напиток алкогольный?');
+    const drinkComposition = prompt('Введите рецепт напитка');
+    return drinkStorage.addValue(drinkName, {drinkAlcohol, drinkComposition});
+};
+document.getElementById('getInfo').onclick = () => {
+    const drinkName = prompt('Введите название напитка');
+    const info = drinkStorage.getValue(drinkName)
+    if (info) {
+        const {drinkAlcohol, drinkComposition} = info;
+        alert(`Напиток: ${drinkName}
+Напиток алкогольный: ${drinkAlcohol} 
+Рецепт приготовления: ${drinkComposition}`
+        );
+    } else alert(`Напиток ${drinkName} не найден`);
+}
+document.getElementById('del').onclick = () => {
+    const drinkName = prompt('Введите название напитка');
+    drinkStorage.deleteValue(drinkName) ?
+        alert(`Информация о напитке ${drinkName} удалена`) :
+        alert(`Напиток ${drinkName} не найден`);
+}
+document.getElementById('getList').onclick = () => {
+    alert(`Имеется информация о напитках: ${drinkStorage.getKeys()}`);
+}

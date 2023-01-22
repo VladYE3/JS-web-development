779ef0ea968f2a21ae7341108172b9e8c038f670
VladYE3 <vlad.verbitsky3@mail.ru>
2023-01-22 22:01:33 +0300

    check + treesum

--- №05 TreeSum/treesum.js
+++ №05 TreeSum/treesum.js
@@ -1,13 +1,21 @@
-let arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
-function treeSum(myArr) {
+let myArr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
+// function treeSum(arr) {
+//     let sum = 0;
+//     arr = arr.flat(Infinity);
+//     for (let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     console.log(sum);
+// }
+// treeSum(myArr);
+function treeSum(arr) {
     let sum = 0;
-    myArr = myArr.flat(Infinity);
-    for (let i = 0; i < myArr.length; i++){
-        sum += myArr[i];
+    for (let i = 0; i < arr.length; i++) {
+        sum += Array.isArray(arr[i]) ? treeSum(arr[i]) : arr[i];
     }
-    console.log(sum);
+    return sum;
 }
-treeSum(arr);
+console.log(treeSum(myArr));
 
 
 
